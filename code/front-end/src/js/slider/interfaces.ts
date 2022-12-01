b26ff29b2a47d2158798d2e2bfd1517cd90ccc08
carlos garcia <chenzhen.dev@gmail.com>
2022-12-01 10:33:56 -0300

    Arrange composition of directories...

---
+++ code/front-end/src/js/slider/interfaces.ts
@@ -0,0 +1,26 @@
+export interface Iitems {
+  image: string;
+  title: string;
+  desc: string;
+  width: number;
+  height: number;
+  order: number;
+}
+
+export interface Icontrols {
+  type: string;
+  elem: string;
+  label: string;
+  order: number;
+}
+
+export interface Islide {
+    container: HTMLElement;
+    itemsList: Iitems[];
+    time: number;
+    index: number;
+    slideinterval: number
+    translate: number
+    progressTimer: number
+}
+
