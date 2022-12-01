77afb109ed511899913e85f6b292612762abade5
carlos garcia <chenzhen.dev@gmail.com>
2022-12-01 12:59:04 -0300

    Remove files and dir unnecessary...

--- code/front-end/src/js/slider/Slider.ts
+++
@@ -1,8 +0,0 @@
-import { Iitems } from "./interfaces";
-import { Banner } from "./Banner.js";
-
-export class Slider extends Banner {
-  constructor(wrap: HTMLElement | null, itemsList: Iitems[], time: number) {
-    super(wrap, itemsList, time);
-  }
-}
